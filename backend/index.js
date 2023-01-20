94b52a4d237c51d93f44060cc6bf04349afd9026
DaniPineda7687 <danielspineda87@gmail.com>
2023-01-20 17:45:17 -0500

    Conexion a la base de datos MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,19 @@
 import express from "express";
-
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
 const app = express();
+
+dotenv.config();
+mongoose.connect(process.env.ATLAS_URI)
+    .then(()=>{
+        console.log("Conectado con exito a la bd")
+    })
+    .catch((err)=>{
+        console.log("Error al conectar la bd",err)
+    })
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors())
